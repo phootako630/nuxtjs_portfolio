39c5324e8897dda7aaa8f8119f0522e4d285a453
ivanzhang <ivanzhang220820@gmail.com>
2023-03-27 22:04:22 -0400

    Add images and a few components

---
+++ components/BackToTop.vue
@@ -0,0 +1,28 @@
+<template>
+
+</template>
+
+<script>
+import feather from "feather-icons";
+export default {
+  components: {},
+  data: () => {
+    return {
+      userScrollPosition: 0,
+    };
+  },
+  computed: {
+    isScrolled() {
+      return this.userScrollPosition > 100;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScrollPosition);
+    feather.replace();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
