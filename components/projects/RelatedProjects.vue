b0bde92e31f9245ce5a1fa190f4538ea8a361fc7
ivanzhang <ivanzhang220820@gmail.com>
2023-03-30 22:37:18 -0400

    Added the related project components

---
+++ components/projects/RelatedProjects.vue
@@ -0,0 +1,70 @@
+<template>
+  <div
+    class="
+      mt-10
+      pt-10
+      sm:pt-14 sm:mt-20
+      border-t-2 border-primary-light
+      dark:border-secondary-dark
+    "
+  >
+    <p
+      class="
+        font-general-regular
+        text-primary-dark
+        dark:text-primary-light
+        text-3xl
+        font-bold
+        mb-10
+        sm:mb-14
+        text-left
+      "
+    >
+      {{ relatedProject.relatedProjectsHeading }}
+    </p>
+
+    <div class="grid grid-cols-1 sm:grid-cols-4 gap-10">
+      <div v-for="item in relatedProject.relatedProjects" :key="item.id">
+        <img
+          :src="item.img"
+          class="rounded-xl cursor-pointer"
+          :alt="item.title"
+        />
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      relatedProject: {
+        relatedProjectsHeading: "Related Projects",
+        relatedProjects: [
+          {
+            id: 1,
+            title: "IMDB Clone",
+            img: require("/images/imdb-project.jpg"),
+          },
+          {
+            id: 2,
+            title: "Google Clone",
+            img: require("/images/google-project.png"),
+          },
+          {
+            id: 3,
+            title: "Terminal Portfolio",
+            img: require("/images/terminal_port.jpg"),
+          }
+        ]
+      }
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
